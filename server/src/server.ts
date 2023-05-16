09341de9b370522614acfd3f585233b2b937c0de
Natanaelvich <taelima1997@gmail.com>
2023-05-15 23:06:14 -0300

    add fastfy config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
-console.log('Hello World')
+import fastify from "fastify";
+
+const server = fastify({
+  logger: true,
+});
+
+server.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+server.listen({ port: 3333 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});

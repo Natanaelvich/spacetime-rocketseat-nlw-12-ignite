ae30e5eda01ddccde8d011f54b95ccbdf834d8b1
Natanaelvich <taelima1997@gmail.com>
2023-05-15 23:14:31 -0300

    fix lint errors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,17 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const server = fastify({
   logger: true,
-});
+})
 
-server.get("/", async (request, reply) => {
-  return { hello: "world" };
-});
+server.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
 
 server.listen({ port: 3333 }, (err, address) => {
   if (err) {
-    console.error(err);
-    process.exit(1);
+    console.error(err)
+    process.exit(1)
   }
-  console.log(`Server listening at ${address}`);
-});
+  console.log(`Server listening at ${address}`)
+})

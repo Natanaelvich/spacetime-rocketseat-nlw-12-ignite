f3a61253f3eb19d3b80ad85d1f735693cd6587a6
Natanaelvich <taelima1997@gmail.com>
2023-05-16 19:31:12 -0300

    fix lint errors

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold text-center"> SpaceTime </h1>
+      <h1 className="text-center text-6xl font-bold"> SpaceTime </h1>
     </main>
   )
 }

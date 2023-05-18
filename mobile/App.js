d6b95565123500986a11a637dbc4fab0fde4f830
Natanaelvich <taelima1997@gmail.com>
2023-05-17 22:12:15 -0300

    update with expo router to navigation

--- mobile/App.js
+++
@@ -1,5 +0,0 @@
-import Main from './src'
-
-export default function App() {
-  return <Main />
-}
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {

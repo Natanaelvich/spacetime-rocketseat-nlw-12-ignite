c18ea0444f72830d2adeff39aac90766c3ca79ff
Natanaelvich <taelima1997@gmail.com>
2023-05-17 22:03:09 -0300

    add load fonts

--- mobile/src/index.tsx
+++ mobile/src/index.tsx
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import { styled } from 'nativewind'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from '../src/assets/bg-blur.png'
 import Stripes from '../src/assets/stripes.svg'
@@ -9,6 +15,16 @@ import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 const StyledStripes = styled(Stripes)
 
 const Main = () => {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
     <ImageBackground
       source={blurBg}
@@ -21,10 +37,10 @@ const Main = () => {
         <NLWLogo />
 
         <View className="space-y-2">
-          <Text className="font-title text-center text-2xl leading-tight text-gray-50">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
             Sua cápsula do tempo
           </Text>
-          <Text className="font-body text-center text-base leading-relaxed text-gray-100">
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
             Colecione momentos marcantes da sua jornada e compartilhe (se
             quiser) com o mundo!
           </Text>
@@ -40,7 +56,7 @@ const Main = () => {
         </TouchableOpacity>
       </View>
 
-      <Text className="font-body text-center text-sm leading-relaxed text-gray-200">
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
 

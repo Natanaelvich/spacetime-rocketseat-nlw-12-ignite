8cc32513075b62be695a628fe54b36675c4d1280
Natanaelvich <taelima1997@gmail.com>
2023-05-17 22:27:57 -0300

    add config cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 
 const server = fastify({
   logger: true,
+})
+
+server.register(cors, {
+  origin: true,
 })
 
 server.register(memoriesRoutes)

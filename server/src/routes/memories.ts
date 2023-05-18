ef4ce8f57955af61d224a00e83eb229c628d76ba
Natanaelvich <taelima1997@gmail.com>
2023-05-17 22:26:42 -0300

    add initial routes momories

---
+++ server/src/routes/memories.ts
@@ -0,0 +1,58 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+import { z } from 'zod'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async (request) => {
+    const memories = await prisma.memory.findMany()
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      }
+    })
+  })
+
+  app.get('/memories/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
+  })
+
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: 'adswd-123123-123123-123123',
+      },
+    })
+
+    return memory
+  })
+
+  app.put('/memories/:id', async (request, reply) => {})
+
+  app.delete('/memories/:id', async (request, reply) => {})
+}

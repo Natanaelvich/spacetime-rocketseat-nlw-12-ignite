c5337b616b0ce7c91ddd760257b1abea19d98817
Natanaelvich <taelima1997@gmail.com>
2023-05-18 21:51:13 -0300

    add auth with github

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -7,12 +7,24 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
+import { GITHUB_CLIENT_ID } from '@env'
 import blurBg from '../src/assets/bg-blur.png'
 import Stripes from '../src/assets/stripes.svg'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
+import { useCallback, useEffect } from 'react'
+import { api } from '../src/lib/api'
+import { useRouter } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
 
 const StyledStripes = styled(Stripes)
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: `https://github.com/settings/connections/applications/${GITHUB_CLIENT_ID}`,
+}
 
 const Main = () => {
   const [hasLoadedFonts] = useFonts({
@@ -20,6 +32,41 @@ const Main = () => {
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+  const router = useRouter()
+
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: GITHUB_CLIENT_ID,
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'spacetime',
+      }),
+    },
+    discovery,
+  )
+
+  const handleGithubOAuthCode = useCallback(
+    async (code: string) => {
+      const response = await api.post('/register', {
+        code,
+      })
+
+      const { token } = response.data
+
+      await SecureStore.setItemAsync('token', token)
+
+      router.push('/memories')
+    },
+    [router],
+  )
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOAuthCode(code)
+    }
+  }, [handleGithubOAuthCode, response])
 
   if (!hasLoadedFonts) {
     return null
@@ -49,6 +96,7 @@ const Main = () => {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança

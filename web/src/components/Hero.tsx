05ae5ef4d0164987b1890f57a22766b30e022fcd
Natanaelvich <taelima1997@gmail.com>
2023-05-19 20:10:27 -0300

    update navigation to new momory

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -19,7 +19,7 @@ export function Hero() {
 
       <a
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-        href=""
+        href="/memories/new"
       >
         CADASTRAR LEMBRANÇA
       </a>

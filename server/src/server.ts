41cfef74d27773d228f8cbe48a1251e8f48e5d4b
Natanaelvich <taelima1997@gmail.com>
2023-05-19 20:25:56 -0300

    add config upload multipart and routes upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,21 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { resolve } from 'node:path'
+import { uploadRoutes } from './routes/upload'
 
 const server = fastify({
   logger: true,
+})
+
+server.register(multipart)
+
+server.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
 })
 
 server.register(cors, {
@@ -17,6 +27,7 @@ server.register(jwt, {
 })
 
 server.register(authRoutes)
+server.register(uploadRoutes)
 server.register(memoriesRoutes)
 
 server.listen({ port: 3333, host: '0.0.0.0' }, (err, address) => {

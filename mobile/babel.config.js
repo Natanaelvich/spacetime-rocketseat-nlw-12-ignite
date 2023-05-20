df6a48be348f4342ce61d4b63a5df788c9927e03
Natanaelvich <taelima1997@gmail.com>
2023-05-20 10:38:51 -0300

    add config babel reanimated

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      'react-native-reanimated/plugin',
       'module:react-native-dotenv',
       'nativewind/babel',
       require.resolve('expo-router/babel'),
